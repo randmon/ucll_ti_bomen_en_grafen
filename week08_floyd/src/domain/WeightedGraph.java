4cc6bbf6e24578326fc96525cddc2379a1b990cd
randmon <ccmarques98@gmail.com>
2021-04-25 18:51:51 +0200

    week 8

--- week08_floyd/src/domain/WeightedGraph.java
+++ week08_floyd/src/domain/WeightedGraph.java
@@ -22,24 +22,53 @@ public class WeightedGraph {
     }
 
     public int[][] getPointerMatrix() {
-        int[][] pointerMatrix = new int[getAantalKnopen()][getAantalKnopen()];
-        double[][] DMatrix = this.gewichtenMatrix.clone();
+        int aantalKnopen = getAantalKnopen();
+        int[][] P = new int[aantalKnopen][aantalKnopen];
+        double[][] D = new double[aantalKnopen][];
+        for (int k = 0; k < aantalKnopen; ++k) {
+            D[k] = this.gewichtenMatrix[k].clone();
+        }
 
-
-		return pointerMatrix;
+        //elke knoop toevoegen als tussenstation
+        for (int station = 0; station < aantalKnopen; ++station) {
+            //van elke knoop (rij)
+            for (int i = 0; i < aantalKnopen; ++i) {
+                //naar elke knoop (kolom)
+                for (int j = 0; j < aantalKnopen; ++j) {
+                    double som = D[i][station] + D[station][j];
+                    if (som < D[i][j]) {
+                        D[i][j] = som;
+                        P[i][j] = station+1;
+                    }
+                }
+            }
+        }
+		return P;
 	}
 
-	public List<Integer> getShortestPath(int i, int j, int[][] pointer) {
-		List<Integer> res = new ArrayList<>();
+	public List<Integer> getShortestPath(int from, int to, int[][] P) {
+		List<Integer> pad = new ArrayList<>();
+		if (from == to) return pad;
 
+		int h = P[from-1][to-1];
+        if (h == 0) {
+            pad.add(from);
+            pad.add(to);
+        } else {
 
-		return res;
+            pad = getShortestPath(from, h, P);
+            pad.remove(pad.size()-1);
+            pad.addAll(getShortestPath(h, to, P));
+        }
+		return pad;
 
 	}
 
 	public int berekenLengte(List<Integer> pad) {
 		int som = 0;
-	
+		for (int i = 1; i < pad.size(); ++i) {
+		    som += gewichtenMatrix[pad.get(i-1)-1][pad.get(i)-1];
+        }
 		return som;
 	}
 
